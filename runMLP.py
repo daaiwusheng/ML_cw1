845153b616d2e2d2c0e00f6b19aa20956e0e25ed
YuWang(Steven) <daaiwusheng003@gmail.com>
2021-11-29 19:09:46 +0000

    momentom用法要注意

--- runMLP.py
+++ runMLP.py
@@ -1,4 +1,4 @@
-from mlp import MLP
+from mlp_w import MLP
 import numpy as np
 
 import pickle, gzip
@@ -8,7 +8,7 @@ tset, vset, teset = pickle.load(f, encoding='latin1')
 print(tset[0].shape, vset[0].shape, teset[0].shape)
 f.close()
 
-tread = 1000
+tread = 9000
 train_in = tset[0][:tread, :]
 
 # This is a little bit of work -- 1 of N encoding
@@ -24,6 +24,6 @@ test_tgt = np.zeros((teread, 10))
 for i in range(teread):
     test_tgt[i, teset[1][i]] = 1
 
-sizes = [784, 5, 5, 10]  # 784 is the number of pixels of the images and 10 is the number of classes
+sizes = [784, 15, 15, 10]  # 784 is the number of pixels of the images and 10 is the number of classes
 classifier = MLP(sizes, beta=5, momentum=0.9)
-classifier.train(train_in, train_tgt, 0.00001, 10000)
+classifier.train(train_in, train_tgt, 0.01, 10000)
